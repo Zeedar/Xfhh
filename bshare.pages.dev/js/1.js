5931e5deb737ae18c76c10894bd522340cf68764
Zeedar <49328914+Zeedar@users.noreply.github.com>
2024-12-30 01:37:20 +0200

    Add files via upload

---
+++ bshare.pages.dev/js/1.js
@@ -0,0 +1,7 @@
+        var text1 = "https://is.gd/Blaban_378",
+            text2 = " ",
+            text3 = " 👉 ",
+            error = "هناك خطأ!\nالمشاركات غير محسوبة ربما شاركته مع نفس الصديق او المجموعة اكثر من مرة، من فضلك اعد المشاركة.",
+            cpa = "https://www.groupmagtrop.com/?sl=5766072-66a2b&amp;pub_click_id={External_ID_from_traffic_source}&amp;site={subID}&amp;pub_sub_id={sub_subID}",
+            saved = "MAMA7lwa",
+            share = "whatsapp://send?text=" + text1;
